3a031cb98af5493fb99303c30fd45c78761169bc
Greg Corbett <greg.corbett@stfc.ac.uk>
2018-06-05 15:25:06 +0100

    Add a script to also save monitoring data
    - the idea being a cron job would run this small script frequently

---
+++ bin/save_monitoring_data.py
@@ -0,0 +1,17 @@
+"""This file contains a small script to save Rancher monitoring data."""
+from rac import APIParser
+
+AGENT_LOG_PATH = "agent_log.example"
+ELASTIC_HOST = "localhost"
+ELASTIC_PORT = "9200"
+
+RANCHER_HOST = "localhost"
+RANCHER_PORT = 8080
+RANCHER_API_VERSION = "v1"
+
+if __name__ == "__main__":
+    API_PARSER = APIParser(RANCHER_HOST, RANCHER_PORT,
+                           RANCHER_API_VERSION,
+                           ELASTIC_HOST, ELASTIC_PORT)
+
+    API_PARSER.save_monitoring_data()
