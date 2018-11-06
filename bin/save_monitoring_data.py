397dbdc7e72efd5904424eb6d5e37337721b48e4
Greg Corbett <greg.corbett@stfc.ac.uk>
2018-11-06 15:36:50 +0000

    Allow config of save_monitoring_data via cfg files

--- bin/save_monitoring_data.py
+++ bin/save_monitoring_data.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python2
 
 """This file contains a small script to save Rancher monitoring data."""
+from optparse import OptionParser
 from rac import APIParser
+
+try:
+    # Import Python 3 configparser module
+    import configparser
+except ImportError:
+    # Import Python 2 version of the module, but alias it to match the
+    # Python 3 version for simplicities sake later on when using the module.
+    import ConfigParser as configparser
 
 AGENT_LOG_PATH = "agent_log.example"
 ELASTIC_HOST = "localhost"
@@ -12,8 +21,30 @@ RANCHER_PORT = 8080
 RANCHER_API_VERSION = "v1"
 
 if __name__ == "__main__":
-    API_PARSER = APIParser(RANCHER_HOST, RANCHER_PORT,
-                           RANCHER_API_VERSION,
-                           ELASTIC_HOST, ELASTIC_PORT)
+
+    optparse = OptionParser()
+    optparse.add_option('-r', '--rancher_config',
+                        help='location of the rancher.cfg file',
+                        default='/etc/apel/rancher.cfg')
+
+    optparse.add_option('-e', '--elastic_config',
+                        help='location of the elastic.cfg file',
+                        default='/etc/apel/elastic.cfg')
+
+    options, unused_args = optparse.parse_args()
+
+    config = configparser.ConfigParser()
+    config.read([options.elastic_config, options.rancher_config])
+
+    elastic_host = config.get('elasticsearch', 'local_host')
+    elastic_port = int(config.get('elasticsearch', 'local_port'))
+
+    rancher_host = config.get('rancher', 'host')
+    rancher_port = int(config.get('rancher', 'port'))
+    rancher_api_version = config.get('rancher', 'api_version')
+
+    API_PARSER = APIParser(rancher_host, rancher_port,
+                           rancher_api_version,
+                           elastic_host, elastic_port)
 
     API_PARSER.save_monitoring_data()
