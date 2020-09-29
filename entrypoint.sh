3b7a3d0c5e3187f5e0b596dc924b04c9dccdc535
Greg Corbett <greg.corbett@stfc.ac.uk>
2020-09-29 15:58:29 +0000

    container: Make the delay between runs configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 # This is a script to be run as the entrypoint to the docker image.
-# It will run `python3 bin/client.py -c conf/client.cfg every
-# minute to capture and send monitoring data. This must be done
-# very frequently, as the monitoring data can disappear when the
-# container it corresponds to is deleted.
+# It will run `python3 bin/client.py -c conf/client.cfg at the
+# configured rate to capture and send monitoring data. The exact
+# depends on the amount of usage you are accounting for and how
+# much you are happy to risk loosing, as the monitoring data
+# can disappear when the container it corresponds to is deleted.
 
 set -eu
 
-minute=`date +"%M"`
-
 while true
 do
-    # Detect if a minute has passed.
-    new_minute=`date +"%M"`
-    if [ $minute != $new_minute ]; then
-      # Save monitoring data.
-      python3 bin/client.py -c conf/client.cfg
-      minute=$new_minute
-    fi
+    # Publish
+    python3 bin/client.py -c conf/client.cfg
+    # Wait the configured time before publishing again.
+    sleep $PUBLISH_FREQUENCY
 done
